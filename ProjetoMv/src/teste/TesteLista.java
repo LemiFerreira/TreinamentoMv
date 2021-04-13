783153e07f223526dd7a6cc9fc30bf33b6184e69
LemiFerreira <60901983+LemiFerreira@users.noreply.github.com>
2021-04-12 22:30:58 -0300

    att do testeLista

--- ProjetoMv/src/teste/TesteLista.java
+++ ProjetoMv/src/teste/TesteLista.java
@@ -1,12 +1,38 @@
 package teste;
 
+import java.util.ArrayList;
+import java.util.List;
 
-
-
+import entidade.Pessoa;
 
 public class TesteLista {
 
-	List(Pessoa) listpessoa = new ArrayList<Pessoa>();
-	for
-	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Pessoa> listaPessoa = new ArrayList<Pessoa>();
+		
+		Pessoa p = new Pessoa();
+		p.setNome("Icaro");
+		p.setCpf("1234");
+		p.setIdade(50);
+		p.setSexo("M");
+		
+		Pessoa pp = new Pessoa();
+		pp.setNome("Lemi");
+		pp.setCpf("5678");
+		pp.setIdade(30);
+		pp.setSexo("M");
+		
+		listaPessoa.add(p);
+		listaPessoa.add(pp);
+		
+		
+		for(Pessoa pessoa : listaPessoa) {
+			System.out.println(pessoa.getNome() + " , idade: "+ pessoa.getIdade());
+		}
+		
+		
+		
+	}
+
 }
